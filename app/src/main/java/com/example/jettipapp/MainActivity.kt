1b1c8c4b3c0c6b708031aff9b847eed0599357d7
v-kuper <vkuper7@gmail.com>
2025-02-14 00:58:10 +0300

    feat: add calculateTotalPerson

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -7,16 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
@@ -27,10 +24,11 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -44,6 +42,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettipapp.components.InputField
 import com.example.jettipapp.ui.theme.JetTipAppTheme
+import com.example.jettipapp.util.calculateTotalPerson
+import com.example.jettipapp.util.calculateTotalTip
 import com.example.jettipapp.widgest.RoundIconButton
 
 class MainActivity : ComponentActivity() {
@@ -52,10 +52,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApp {
-                Column {
-                    TopHeader()
-                    MainContent()
-                }
+                MainContent()
             }
         }
     }
@@ -71,14 +68,37 @@ fun MyApp(content: @Composable (Modifier) -> Unit) {
 }
 
 @Composable
-fun TopHeader(totalPerPerson: Double = 0.0) {
-    Surface(modifier = Modifier
+fun MainContent() {
+    val splitByState = remember { mutableStateOf(1) }
+    val range = IntRange(start = 1, endInclusive = 100)
+
+    val tipAmountState = remember { mutableStateOf(0.0) }
+
+    val totalPerPersonState = remember { mutableStateOf(0.0) }
+
+    Column(modifier = Modifier.padding(all = 12.dp)) {
+        TopHeader(totalPerPerson = totalPerPersonState.value)
+        BillForm(
+            splitByState = splitByState,
+            range = range,
+            tipAmountState = tipAmountState,
+            totalPerPersonState = totalPerPersonState
+        ) { billAmt ->
+            Log.d("AMT", "MainContent: $billAmt")
+        }
+    }
+}
+
+@Composable
+fun TopHeader(modifier: Modifier = Modifier, totalPerPerson: Double = 0.0) {
+    Surface(modifier = modifier
         .fillMaxWidth()
+        .padding(15.dp)
         .height(150.dp)
         .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
         color = Color(0xFFE9D7F7)
     ) {
-        Column(modifier = Modifier.padding(12.dp),
+        Column(modifier = modifier.padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -92,37 +112,43 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     }
 }
 
-@Composable
-fun MainContent() {
-    BillForm(modifier = Modifier) { billAmt ->
-        Log.d("AMT", "MainContent: $billAmt")
-    }
-}
 
 @Composable
 fun BillForm(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
+    range: IntRange = 1..100,
+    splitByState: MutableState<Int>,
+    tipAmountState: MutableState<Double>,
+    totalPerPersonState: MutableState<Double>,
     onValChange: (value: String) -> Unit = {}
 ) {
     val totalBillState = remember { mutableStateOf("") }
-    val validState = remember(totalBillState.value) {
-        totalBillState.value.trim().isNotEmpty()
-    }
-    val keyboardController = LocalSoftwareKeyboardController.current
+    val validState = remember(totalBillState.value) { totalBillState.value.trim().isNotEmpty() }
 
-    Surface(modifier = modifier
-        .padding(2.dp)
-        .fillMaxWidth(),
-        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val sliderPositionState = remember { mutableStateOf(0f) }
+    val tipPercentage = (sliderPositionState.value * 100).toInt()
+
+    fun updateValues() {
+        val bill = totalBillState.value.toDoubleOrNull() ?: 0.0
+        tipAmountState.value = calculateTotalTip(bill, tipPercentage)
+        totalPerPersonState.value = calculateTotalPerson(bill, splitByState.value, tipPercentage)
+    }
+
+    Surface(
+        modifier = modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.LightGray)
     ) {
         Column(
-            modifier = modifier.padding(6.dp),
+            modifier = Modifier.padding(6.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
             InputField(
-                modifier = modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 valueState = totalBillState,
                 labelId = "Enter Bill",
                 enabled = true,
@@ -132,55 +158,84 @@ fun BillForm(
                     if (!validState) return@KeyboardActions
                     onValChange(totalBillState.value.trim())
                     keyboardController?.hide()
+                    updateValues()
                 }
             )
 
             if (validState) {
                 Row(
-                    modifier = modifier.padding(3.dp),
+                    modifier = Modifier.padding(3.dp),
                     horizontalArrangement = Arrangement.Start
                 ) {
-                    Text(text = "Split",
-                        modifier = modifier.align(alignment = Alignment.CenterVertically)
-                    )
-                    Spacer(modifier = modifier.width(120.dp))
+                    Text(text = "Split", modifier = Modifier.align(Alignment.CenterVertically))
+                    Spacer(Modifier.width(120.dp))
+
                     Row(
-                        modifier = modifier.padding(horizontal = 3.dp),
+                        modifier = Modifier.padding(horizontal = 3.dp),
                         horizontalArrangement = Arrangement.End,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         RoundIconButton(
                             imageVector = Icons.Default.Remove,
-                            onClick = {  }
+                            onClick = {
+                                if (splitByState.value > range.first) {
+                                    splitByState.value -= 1
+                                    updateValues()
+                                }
+                            }
                         )
                         Text(
-                            text = totalBillState.value,
-                            modifier = modifier.padding(horizontal = 9.dp)
+                            text = "${splitByState.value}",
+                            modifier = Modifier.padding(horizontal = 9.dp)
                         )
                         RoundIconButton(
                             imageVector = Icons.Default.Add,
-                            onClick = {  }
+                            onClick = {
+                                if (splitByState.value < range.last) {
+                                    splitByState.value += 1
+                                    updateValues()
+                                }
+                            }
                         )
                     }
                 }
-            } else {
-                Box() {
 
+                Row(
+                    modifier = Modifier.padding(horizontal = 3.dp, vertical = 12.dp)
+                ) {
+                    Text(text = "Tip", modifier = Modifier.align(Alignment.CenterVertically))
+                    Spacer(Modifier.width(180.dp))
+                    Text(text = "$ ${tipAmountState.value}")
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "$tipPercentage %")
+                    Spacer(Modifier.height(14.dp))
+
+                    Slider(
+                        value = sliderPositionState.value,
+                        onValueChange = { newVal ->
+                            sliderPositionState.value = newVal
+                            updateValues()
+                        },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
                 }
             }
         }
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetTipAppTheme {
         MyApp {
-            Column {
-                TopHeader()
-                MainContent()
-            }
+            MainContent()
         }
     }
 }
